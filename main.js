c9e3b1ffa38866bcd025752b99f94eafde5630bd
EmiliaBanak <148707385+EmiliaBanak@users.noreply.github.com>
2023-11-23 09:06:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const myButton = document.querySelector("#myButton");
 
+// Adding an event listener to the button
+document.getElementById("myButton").addEventListener("click", showAlert);
+function showAlert() {
+  alert("Your message was sent successfully. Thank you! ");
+}
+
+function playClickSound() {
+  let audio = document.getElementById("clickSound");
+  audio.play();
+}
